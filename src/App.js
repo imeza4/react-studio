889e35c13ffd89626920bd1f7cb57bc7e0a2ef06
Isabelle Meza <isabellemeza@eugene.devices.brown.edu>
2024-03-14 19:48:59 -0400

    yes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItems from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -14,15 +15,18 @@ function App() {
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
+      <h1>My Bakery!</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <p>Bakery Item {BakeryItems}</p> // replace with BakeryItem component
+
+        
       ))}
 
       <div>
         <h2>Cart</h2>
         {/* TODO: render a list of items in the cart */}
+
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,10 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItems(props) {
+
+    return (
+      <div className="BakeryItems">
+        <img alt="image of bakery item" src={props.image}/>
+        <h1>{props.name}</h1> 
+      </div>
+    );
+  }
